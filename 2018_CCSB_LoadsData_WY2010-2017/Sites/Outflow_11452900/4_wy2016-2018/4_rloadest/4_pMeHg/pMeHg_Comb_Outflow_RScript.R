ab47dbd82787ed1a4f3888b8083b0616fb41d733
Rose <slrose@usgs.gov>
2019-10-20 17:05:23 -0700

    CCSB Loads, Outflow, wy2016-18, Sand --- replaced Sand Sigma Plot PDF files and replaced plots for Sand on the Power Points;  Outflow, wy2016-18, pMeHg, fMeHg, wwMeHg: updated Q and WQ worksheet; updated model results; created a second (Part 2) Sigma Plot file, updated the older Sigma Plot file; added new PDF files to the Sigma Plot folder; updated the PowerPoint files; replaced the rloadest folders; updated the Loads Progress Sheet.

--- 2018_CCSB_LoadsData_WY2010-2017/Sites/Outflow_11452900/4_wy2016-2018/4_rloadest/4_pMeHg/pMeHg_Comb_Outflow_RScript.R
+++ 2018_CCSB_LoadsData_WY2010-2017/Sites/Outflow_11452900/4_wy2016-2018/4_rloadest/4_pMeHg/pMeHg_Comb_Outflow_RScript.R
@@ -16,31 +16,94 @@ library(boot)
 library(KernSmooth)
 library(lattice)
 
-pMeHg_Outflow<-importRDB("pMeHg_Comb_OutflowR.txt") #Use the Combined Outflow (Outlet+Spillway) for modeling the Outflow. Choose the best model, then use only Outflow data on the next script
-#The data frame is created by the function importRDB. 
-#The call above brings the constituent data and the instantaneous flow data into the script.
+pMeHg_Outflow<-importRDB("pMeHg_Comb_OutflowR.txt")
+OutflowQ<-importRDB("Comb_OutflowQR.txt")
 
-pMeHg_Outflowm1 <- loadReg(pMeHg ~model(1), data = pMeHg_Outflow, flow="Flow", dates = "Dates" ,conc.units="ng/L" , station = "CCSB-Yolo")
+#A data frame is created by the function importRDB. 
+#The call on line 19 brings the constituent data and the instantaneous flow data (Comb_Outflow Q = Outlet Inst Q + Spillway Inst Q)into the script.
+#The call on line 20 brings in the daily Comb_Outflow (Outlet Daily Q +Spillway Daily Q) data. 
+
+#***************************************************Do the following if you have censored data:*****************************************************
+#(1)Create vector of detection limit
+
+det<-as.numeric(array(.1,dim(pMeHg_Outflow)[1]))
+
+#(2)Create lcens object- as.lcens(values, detlim, censor.codes)
+cen_data<-as.lcens(pMeHg_Outflow$PpMeHg,det,pMeHg_Outflow$RpMeHg)
+
+#(3)Create input data frame with censored data object replacing last 2 columns
+cen_frame<-data.frame(pMeHg_Outflow[,1:3],cen_data)
+
+#(4)Use this code to run models for censored data:
+
+pMeHg_Outflowm1 <- loadReg(cen_data ~model(1), data = cen_frame, flow="Flow", dates = "Dates" ,conc.units="ng/L" , station = "CCSB-Outflow")
 pMeHg_Outflowm1
-pMeHg_Outflowm2 <- loadReg(pMeHg ~model(2), data = pMeHg_Outflow, flow="Flow", dates = "Dates" ,conc.units="ng/L" , station = "CCSB-Yolo")
+pMeHg_Outflowm2 <- loadReg(cen_data ~model(2), data = cen_frame, flow="Flow", dates = "Dates" ,conc.units="ng/L" , station = "CCSB-Outflow")
 pMeHg_Outflowm2
-pMeHg_Outflowm3 <- loadReg(pMeHg ~model(3), data = pMeHg_Outflow, flow="Flow", dates = "Dates" ,conc.units="ng/L" , station = "CCSB-Yolo")
+pMeHg_Outflowm3 <- loadReg(cen_data ~model(3), data = cen_frame, flow="Flow", dates = "Dates" ,conc.units="ng/L" , station = "CCSB-Outflow")
 pMeHg_Outflowm3
-pMeHg_Outflowm4 <- loadReg(pMeHg ~model(4), data = pMeHg_Outflow, flow="Flow", dates = "Dates" ,conc.units="ng/L" , station = "CCSB-Yolo")
+pMeHg_Outflowm4 <- loadReg(cen_data ~model(4), data = cen_frame, flow="Flow", dates = "Dates" ,conc.units="ng/L" , station = "CCSB-Outflow")
 pMeHg_Outflowm4
-pMeHg_Outflowm5 <- loadReg(pMeHg ~model(5), data = pMeHg_Outflow, flow="Flow", dates = "Dates" ,conc.units="ng/L" , station = "CCSB-Yolo")
+pMeHg_Outflowm5 <- loadReg(cen_data ~model(5), data = cen_frame, flow="Flow", dates = "Dates" ,conc.units="ng/L" , station = "CCSB-Outflow")
 pMeHg_Outflowm5
-pMeHg_Outflowm6 <- loadReg(pMeHg ~model(6), data = pMeHg_Outflow, flow="Flow", dates = "Dates" ,conc.units="ng/L" , station = "CCSB-Yolo")
+pMeHg_Outflowm6 <- loadReg(cen_data ~model(6), data = cen_frame, flow="Flow", dates = "Dates" ,conc.units="ng/L" , station = "CCSB-Outflow")
 pMeHg_Outflowm6
-pMeHg_Outflowm7 <- loadReg(pMeHg ~model(7), data = pMeHg_Outflow, flow="Flow", dates = "Dates" ,conc.units="ng/L" , station = "CCSB-Yolo")
+pMeHg_Outflowm7 <- loadReg(cen_data ~model(7), data = cen_frame, flow="Flow", dates = "Dates" ,conc.units="ng/L" , station = "CCSB-Outflow")
 pMeHg_Outflowm7
-pMeHg_Outflowm8 <- loadReg(pMeHg ~model(8), data = pMeHg_Outflow, flow="Flow", dates = "Dates" ,conc.units="ng/L" , station = "CCSB-Yolo")
+pMeHg_Outflowm8 <- loadReg(cen_data ~model(8), data = cen_frame, flow="Flow", dates = "Dates" ,conc.units="ng/L" , station = "CCSB-Outflow")
 pMeHg_Outflowm8
-pMeHg_Outflowm9 <- loadReg(pMeHg ~model(9), data = pMeHg_Outflow, flow="Flow", dates = "Dates" ,conc.units="ng/L" , station = "CCSB-Yolo")
+pMeHg_Outflowm9 <- loadReg(cen_data ~model(9), data = cen_frame, flow="Flow", dates = "Dates" ,conc.units="ng/L" , station = "CCSB-Outflow")
 pMeHg_Outflowm9
+
+#****************************Use this code to run models for uncensored data, comment out the censored data code(above):******************************
+
+#pMeHg_Outflowm1 <- loadReg(pMeHg ~model(1), data = test_frame, flow="Flow", dates = "Dates" ,conc.units="ng/L" , station = "CCSB-Outflow")
+#pMeHg_Outflowm1
+#pMeHg_Outflowm2 <- loadReg(pMeHg ~model(2), data = pMeHg_Outflow, flow="Flow", dates = "Dates" ,conc.units="ng/L" , station = "CCSB-Outflow")
+#pMeHg_Outflowm2
+#pMeHg_Outflowm3 <- loadReg(pMeHg ~model(3), data = pMeHg_Outflow, flow="Flow", dates = "Dates" ,conc.units="ng/L" , station = "CCSB-Outflow")
+#pMeHg_Outflowm3
+#pMeHg_Outflowm4 <- loadReg(pMeHg ~model(4), data = pMeHg_Outflow, flow="Flow", dates = "Dates" ,conc.units="ng/L" , station = "CCSB-Outflow")
+#pMeHg_Outflowm4
+#pMeHg_Outflowm5 <- loadReg(pMeHg ~model(5), data = pMeHg_Outflow, flow="Flow", dates = "Dates" ,conc.units="ng/L" , station = "CCSB-Outflow")
+#pMeHg_Outflowm5
+#pMeHg_Outflowm6 <- loadReg(pMeHg ~model(6), data = pMeHg_Outflow, flow="Flow", dates = "Dates" ,conc.units="ng/L" , station = "CCSB-Outflow")
+#pMeHg_Outflowm6
+#pMeHg_Outflowm7 <- loadReg(pMeHg ~model(7), data = pMeHg_Outflow, flow="Flow", dates = "Dates" ,conc.units="ng/L" , station = "CCSB-Outflow")
+#pMeHg_Outflowm7
+#pMeHg_Outflowm8 <- loadReg(pMeHg ~model(8), data = pMeHg_Outflow, flow="Flow", dates = "Dates" ,conc.units="ng/L" , station = "CCSB-Outflow")
+#pMeHg_Outflowm8
+#pMeHg_Outflowm9 <- loadReg(pMeHg ~model(9), data = pMeHg_Outflow, flow="Flow", dates = "Dates" ,conc.units="ng/L" , station = "CCSB-Outflow")
+#pMeHg_Outflowm9
 
 #These objects of class "loadReg" are created.
 #A list in R allows you to gather a variety of objects under one name (that is, the name of the list) in an ordered way. 
 #These objects can be matrices, vectors, data frames, even other lists, etc. It is not even required that these objects are related to each other in any way.
 #When the models are run (m1-m9), the output will be in the console. These are the stats used to select the best model. 
 
+plot(pMeHg_Outflowm1,ann=FALSE)
+title(main = "4_11452900_pMeHg_m1 Response vs Fitted Values",xlab = "Fitted Values",ylab = "Response Values")
+plot(pMeHg_Outflowm1,which = 2,set.up = F)
+title(main = "4_11452900_pMeHg_m1 Residuals vs Fitted Values")
+plot(pMeHg_Outflowm1,which = 3,set.up = F)
+title(main = "4_11452900_pMeHg_m1 Assessing Heteroscedasticity") 
+plot(pMeHg_Outflowm1,which = 4,set.up = F)
+title(main = "4_11452900_pMeHg_m1 Correlogram of Samples")
+plot(pMeHg_Outflowm1,which = 5,set.up = F)
+title(main="4_11452900_pMeHg_m1 Normal Discharge")
+plot(pMeHg_Outflowm1,which = 6,set.up = F)
+title(main="4_11452900_pMeHg_m1 Box Plot of Loads")
+
+#The functions above plot the data using the chosen best model and add a title and labels to the plot.
+
+#Change the model number in lines 66-69 before running the loads predictions. This should be the best model. 
+
+pMeHg_Outflow_load<-predLoad(pMeHg_Outflowm1,OutflowQ,load.units="kg",by="water year",allow.incomplete = TRUE,conf.int = 0.95,print = TRUE)
+write.csv(pMeHg_Outflow_load,"4_Outflow_pMeHg_m1_Flux_Annual_uncorr.csv")
+pMeHg_Outflow_load_day<-predLoad(pMeHg_Outflowm1, OutflowQ,load.units = "kg",by="day",allow.incomplete = TRUE,conf.int = 0.90,print = TRUE)
+write.csv(pMeHg_Outflow_load_day,"4_Outflow_pMeHg_m1_Flux_Daily_uncorr.csv")
+
+#Lines 66 and 68 create data frames that use the function predLoad. 
+#Description of predLoad: Estimate loads from a rating-curve model from loadReg for a new data frame, aggregating the loads by specified time periods.
+#Lines 67 and 69 write the data frames to a .csv file. Important note: The file must be intentionally saved as a CSV file- type ".csv".
+#file.choose() lets the user select the location for the .csv files.
