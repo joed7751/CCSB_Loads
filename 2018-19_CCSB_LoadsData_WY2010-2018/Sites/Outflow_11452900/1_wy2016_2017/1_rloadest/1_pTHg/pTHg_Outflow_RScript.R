f1cbec42a223fa66e1765391e91de57f1fbaee65
Rose <slrose@usgs.gov>
2019-10-22 23:41:49 -0700

    CCSB Loads, wy2016-17, Spillway: replaced all model results files; replaced rloadest pMeHg and wwMeHg folders (changed from model 4 for both to model 3); replaced all rloadest flux files; updated some rloadest plots; updated the Q and WQ worksheet; changed SSC from model 7 to model 1; added new Spillway Sigma Plot folder, added Powerpoint presentation for Rumsey and Spillway; updated Loads Progress sheet.

---
+++ 2018-19_CCSB_LoadsData_WY2010-2018/Sites/Outflow_11452900/1_wy2016_2017/1_rloadest/1_pTHg/pTHg_Outflow_RScript.R
@@ -0,0 +1,85 @@
+#pTHg Outlet script for retrieving loads models stats (the stats are used to select the best model for this constituent at this site) and loads predictions.
+
+library(akima)
+library(dataRetrieval)
+library(digest)
+library(leaps)
+library(lubridate)
+library(memoise)
+library(rloadest)
+library(smwrBase)
+library(smwrData)
+library(smwrGraphs)
+library(smwrQW)
+library(smwrStats)
+library(boot)
+library(KernSmooth)
+library(lattice)
+
+pTHg_Outlet<-importRDB("pTHg_OutletR.txt")  #Use this file when the best model has been selected. 
+OutletQ<-importRDB("OutletQR.txt")
+
+#These data frames are created by the function importRDB. 
+#The calls above bring the constituent data and the daily flow data into the script.
+
+pTHg_Outletm1 <- loadReg(pTHg ~model(1), data = pTHg_Outlet, flow="Flow", dates = "Dates" ,conc.units="ng/L" , station = "CCSB-Yolo")
+pTHg_Outletm1
+pTHg_Outletm2 <- loadReg(pTHg ~model(2), data = pTHg_Outlet, flow="Flow", dates = "Dates" ,conc.units="ng/L" , station = "CCSB-Yolo")
+pTHg_Outletm2
+pTHg_Outletm3 <- loadReg(pTHg ~model(3), data = pTHg_Outlet, flow="Flow", dates = "Dates" ,conc.units="ng/L" , station = "CCSB-Yolo")
+pTHg_Outletm3
+pTHg_Outletm4 <- loadReg(pTHg ~model(4), data = pTHg_Outlet, flow="Flow", dates = "Dates" ,conc.units="ng/L" , station = "CCSB-Yolo")
+pTHg_Outletm4
+pTHg_Outletm5 <- loadReg(pTHg ~model(5), data = pTHg_Outlet, flow="Flow", dates = "Dates" ,conc.units="ng/L" , station = "CCSB-Yolo")
+pTHg_Outletm5
+pTHg_Outletm6 <- loadReg(pTHg ~model(6), data = pTHg_Outlet, flow="Flow", dates = "Dates" ,conc.units="ng/L" , station = "CCSB-Yolo")
+pTHg_Outletm6
+pTHg_Outletm7 <- loadReg(pTHg ~model(7), data = pTHg_Outlet, flow="Flow", dates = "Dates" ,conc.units="ng/L" , station = "CCSB-Yolo")
+pTHg_Outletm7
+pTHg_Outletm8 <- loadReg(pTHg ~model(8), data = pTHg_Outlet, flow="Flow", dates = "Dates" ,conc.units="ng/L" , station = "CCSB-Yolo")
+pTHg_Outletm8
+pTHg_Outletm9 <- loadReg(pTHg ~model(9), data = pTHg_Outlet, flow="Flow", dates = "Dates" ,conc.units="ng/L" , station = "CCSB-Yolo")
+pTHg_Outletm9
+
+#These objects of class "loadReg" are created.
+#A list in R allows you to gather a variety of objects under one name (that is, the name of the list) in an ordered way. 
+#These objects can be matrices, vectors, data frames, even other lists, etc. It is not even required that these objects are related to each other in any way.
+#When the models are run (m1-m9), the output will be in the console. These are the stats used to select the best model. 
+
+#print(pTHg_Outletm1,brief = FALSE, load.only = FALSE)
+#print(pTHg_Outletm2,brief = FALSE, load.only = FALSE)
+#print(pTHg_Outletm3,brief = FALSE, load.only = FALSE)
+#print(pTHg_Outletm4,brief = FALSE, load.only = FALSE)
+#print(pTHg_Outletm5,brief = FALSE, load.only = FALSE)
+#print(pTHg_Outletm6,brief = FALSE, load.only = FALSE)
+#print(pTHg_Outletm7,brief = FALSE, load.only = FALSE)
+#print(pTHg_Outletm8,brief = FALSE, load.only = FALSE)
+#print(pTHg_Outletm9,brief = FALSE, load.only = FALSE)
+#Commenting these out. These provide some explanations of the data in a longer form. Brief results are printed to console (pTHg_Outletm1-9)
+
+plot(pTHg_Outletm5,ann=FALSE)
+title(main = "11452900_pTHg Response vs Fitted Values",xlab = "Fitted Values",ylab = "Response Values")
+plot(pTHg_Outletm5,which = 2,set.up = F)
+title(main = "11452900_pTHg Residuals vs Fitted Values")
+plot(pTHg_Outletm5,which = 3,set.up = F)
+title(main = "11452900_pTHg Assessing Heteroscedasticity") #Add "of Residuals"?
+plot(pTHg_Outletm5,which = 4,set.up = F)
+title(main = "11452900_pTHg Correlogram of Samples")
+plot(pTHg_Outletm5,which = 5,set.up = F)
+title(main="11452900_pTHg Normal Discharge")
+plot(pTHg_Outletm5,which = 6,set.up = F)
+title(main="11452900_pTHg Box Plot of Loads")
+
+#These functions plot the data using the chosen best model and add a title and labels to the plot.
+
+#Change the model number in lines 77 and 79 before running the loads predictions. This should be the best model. 
+
+pTHg_Outlet_load<-predLoad(pTHg_Outletm5,OutletQ,load.units="kg",by="water year",allow.incomplete = TRUE,conf.int = 0.95,print = TRUE)
+write.csv(pTHg_Outlet_load,"1_Outflow_pTHg_Flux_Annual.csv")
+pTHg_Outlet_load_day<-predLoad(pTHg_Outletm5, OutletQ,load.units = "kg",by="day",allow.incomplete = TRUE,conf.int = 0.90,print = TRUE)
+write.csv(pTHg_Outlet_load_day,"1_Outflow_pTHg_Flux_Daily.csv")
+
+#Lines 77 and 79 create data frames that use the function predLoad. 
+#Description of predLoad: Estimate loads from a rating-curve model from loadReg for a new data frame, aggregating the loads by specified time periods.
+#Lines 78 and 80 write the data frames to a .csv file. Important note: The file must be intentionally saved as a CSV file- type ".csv" at the end when prompted.
+#file.choose() lets the user select the location for the .csv files.
